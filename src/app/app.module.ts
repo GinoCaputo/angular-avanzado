37440a499bb3fcf267c09a01392e50d057fe99e7
Gino Caputo <ginopq@gmail.com>
2021-08-02 10:31:39 -0300

    Feat: configurando recursos + comienzo de estructuracion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BreadcumbsComponent } from './shared/breadcumbs/breadcumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NopagefoundComponent,
+    SidebarComponent,
+    HeaderComponent,
+    BreadcumbsComponent
   ],
   imports: [
     BrowserModule
