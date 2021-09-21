96d20aadf718e43fbad75a8442d1dddc089717c5
Gino Caputo <ginopq@gmail.com>
2021-09-21 09:18:57 -0300

    aplicar scripts index

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
-declare function customInitFunction();
+declare function customInitFunction():any;
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
